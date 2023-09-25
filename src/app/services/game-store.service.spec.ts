49a2e4e3ba31937121b95c096ba5b76d2b092572
fernanman1207 <101334456+fernanman1207@users.noreply.github.com>
2023-09-25 13:14:48 -0300

    empezo a enredarse

--- src/app/services/game-store.service.spec.ts
+++ src/app/services/game-store.service.spec.ts
@@ -1,16 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { inject } from '@angular/core/testing';
+import { SQLiteObject } from '@ionic-native/sqlite/ngx';
 
-import { GameStoreService } from './game-store.service';
 
-describe('GameStoreService', () => {
-  let service: GameStoreService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameStoreService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
--- src/app/services/game-store.service.ts
+++ src/app/services/game-store.service.ts
@@ -75,64 +75,10 @@ export class GameStoreService {
 }
 
 export class Usuario {
-  id: number;
-  nombre: string;
-  email: string;
-  contraseña: string;
+  id!: number;
+  nombre!: string;
+  email!: string;
+  contraseña!: string;
 }
-
-@Injectable({
-  providedIn: 'root'
-})
-export class UsuarioService {
-  private usuarios: Usuario[] = [];
-  private lastId: number = 0;
-
-  constructor() {}
-
-  // Función para crear un nuevo usuario
-  crearUsuario(usuario: Usuario): Usuario {
-    usuario.id = ++this.lastId;
-    this.usuarios.push(usuario);
-    return usuario;
-  }
-
-  // Función para obtener todos los usuarios
-  obtenerUsuarios(): Usuario[] {
-    return this.usuarios;
-  }
-
-  // Función para obtener un usuario por ID
-  obtenerUsuarioPorId(id: number): Usuario {
-    return this.usuarios.find(usuario => usuario.id === id);
-  }
-
-  // Función para actualizar un usuario
-  actualizarUsuario(id: number, usuarioActualizado: Usuario): Usuario {
-    const usuario = this.obtenerUsuarioPorId(id);
-    if (usuario) {
-      usuario.nombre = usuarioActualizado.nombre;
-      usuario.email = usuarioActualizado.email;
-      usuario.contraseña = usuarioActualizado.contraseña;
-      return usuario;
-    }
-    return null;
-  }
-
-  // Función para eliminar un usuario por ID
-  eliminarUsuario(id: number): boolean {
-    const index = this.usuarios.findIndex(usuario => usuario.id === id);
-    if (index !== -1) {
-      this.usuarios.splice(index, 1);
-      return true;
-    }
-    return false;
-  }
-}
-
-
-
-
-
 
 
