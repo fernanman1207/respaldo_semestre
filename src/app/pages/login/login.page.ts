1e7ead9873055de3162953e0ca43e0ca2b7a964e
fernanman1207 <101334456+fernanman1207@users.noreply.github.com>
2023-10-06 13:26:17 -0300

    Update login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,34 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; // Importa el módulo Router
 import { DatabaseService } from 'src/app/services/Database.Service'; // Reemplaza 'ruta-hacia-tu-database-service' con la ruta real a tu servicio DatabaseService
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
-  correo: string | undefined;
-  clave: string | undefined;
+export class LoginPage implements OnInit {
+  [x: string]: any;
+  loginForm: FormGroup | undefined;
+  isSubmitted = false;
 
   constructor(
     private router: Router, // Inyecta el Router para gestionar la navegación
-    private databaseService: DatabaseService // Inyecta el servicio DatabaseService
+    private databaseService: DatabaseService, // Inyecta el servicio DatabaseService
+    private formBuilder: FormBuilder,
   ) {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
-  onLogin() {
-    // Verifica las credenciales en la base de datos
-    // Verifica las credenciales en la base de datos
-    this.databaseService['verificarCredenciales'](this.correo, this.clave)
-      .then((usuario: any): void => {
-        if (usuario) {
-          // Las credenciales son válidas, redirige al usuario a otra página
-          this.router.navigate(['/Inicio']); // Reemplaza '/inicio' con la ruta a la página que desees mostrar después de iniciar sesión
-        } else {
-          // Las credenciales son inválidas, muestra un mensaje de error o realiza alguna acción adecuada
-          console.log('Credenciales inválidas');
-        }
-      });
+  get formControls() {
+    return this.loginForm!.controls;
+  }
+
+  async onSubmit() {
+    this.isSubmitted = true;
+    if (this.loginForm!.valid) {
+      const email = this.formControls['email'].value;
+      const password = this.formControls['password'].value;
+
+      // Aquí deberías llamar a la función de inicio de sesión en tu servicio UsuarioService
+      // Por ejemplo:
+      const usuario = this.databaseService.obtenerUsuarioPorId;
+
+      if (await newFunction(usuario)) {
+        // Inicio de sesión exitoso
+        // Puedes redirigir al usuario a la página de inicio o a la página deseada
+        this.router.navigate(['/home']);
+      } else {
+        // Inicio de sesión fallido
+        // Puedes mostrar un mensaje de error al usuario
+      }
+    }
+
+    function newFunction(usuario: (id: number) => Promise<any>) {
+      return newFunction_1(usuario);
+    }
+
+    function newFunction_1(usuario: (id: number) => Promise<any>) {
+      return usuario();
+    }
   }
 }
