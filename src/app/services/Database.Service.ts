f4ed663cd269dba2137edf614e4c9415c2c22726
fernanman1207 <fe.gomezv@duocuc.cl>
2023-10-10 14:52:48 -0300

    Update Database.Service.ts

--- src/app/services/Database.Service.ts
+++ src/app/services/Database.Service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 import { Usuario } from './usuario';
+import { Producto } from './producto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseService {
-  [x: string]: any;
   private db!: SQLiteObject;
 
   constructor(private sqlite: SQLite) {
@@ -22,6 +22,7 @@ export class DatabaseService {
       .then((db: SQLiteObject) => {
         this.db = db;
         this.createTableUsuarios();
+        this.createTableProductos();
       })
       .catch((error: any) => {
         console.error('Error al abrir la base de datos: ', error);
@@ -48,10 +49,28 @@ export class DatabaseService {
       );
   }
 
-  // Agregar métodos para realizar operaciones CRUD en la tabla "usuarios"
-  // Por ejemplo, métodos para agregar, listar, actualizar y eliminar usuarios.
+  private createTableProductos() {
+    this.db
+      .executeSql(
+        `
+      CREATE TABLE IF NOT EXISTS productos (
+        id_prod INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        descripcion TEXT,
+        precio REAL,
+        stock INTEGER,
+        foto TEXT
+      )`,
+        []
+      )
+      .then(() => console.log('Tabla de productos creada'))
+      .catch((error: any) =>
+        console.error('Error al crear la tabla de productos: ', error)
+      );
+  }
 
-  // Función para agregar un usuario a la tabla "usuarios"
+  // Métodos CRUD para la tabla "usuarios"
+
   agregarUsuario(usuario: Usuario) {
     const { nombre, correo, clave, pregunta_secreta, respuesta_secreta } = usuario;
     return this.db.executeSql(
@@ -60,7 +79,6 @@ export class DatabaseService {
     );
   }
 
-  // Función para obtener todos los usuarios de la tabla "usuarios"
   obtenerUsuarios() {
     return this.db.executeSql('SELECT * FROM usuarios', []).then((data) => {
       const usuarios = [];
@@ -71,7 +89,6 @@ export class DatabaseService {
     });
   }
 
-  // Función para obtener un usuario por su ID
   obtenerUsuarioPorId(id: number) {
     return this.db
       .executeSql('SELECT * FROM usuarios WHERE id_usuario = ?', [id])
@@ -83,7 +100,6 @@ export class DatabaseService {
       });
   }
 
-  // Función para actualizar un usuario en la tabla "usuarios" por su ID
   actualizarUsuario(id: number, usuario: Usuario) {
     const { nombre, correo, clave, pregunta_secreta, respuesta_secreta } = usuario;
     return this.db.executeSql(
@@ -92,10 +108,50 @@ export class DatabaseService {
     );
   }
 
-  // Función para eliminar un usuario de la tabla "usuarios" por su ID
   eliminarUsuario(id: number) {
     return this.db.executeSql('DELETE FROM usuarios WHERE id_usuario = ?', [id]);
   }
 
-  
+  // Métodos CRUD para la tabla "productos"
+
+  agregarProducto(producto: Producto) {
+    const { nombre, descripcion, precio, stock, foto } = producto;
+    return this.db.executeSql(
+      'INSERT INTO productos (nombre, descripcion, precio, stock, foto) VALUES (?, ?, ?, ?, ?)',
+      [nombre, descripcion, precio, stock, foto]
+    );
+  }
+
+  obtenerProductos() {
+    return this.db.executeSql('SELECT * FROM productos', []).then((data) => {
+      const productos = [];
+      for (let i = 0; i < data.rows.length; i++) {
+        productos.push(data.rows.item(i));
+      }
+      return productos;
+    });
+  }
+
+  obtenerProductoPorId(id: number) {
+    return this.db
+      .executeSql('SELECT * FROM productos WHERE id_prod = ?', [id])
+      .then((data) => {
+        if (data.rows.length > 0) {
+          return data.rows.item(0);
+        }
+        return null;
+      });
+  }
+
+  actualizarProducto(id: number, producto: Producto) {
+    const { nombre, descripcion, precio, stock, foto } = producto;
+    return this.db.executeSql(
+      'UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, stock = ?, foto = ? WHERE id_prod = ?',
+      [nombre, descripcion, precio, stock, foto, id]
+    );
+  }
+
+  eliminarProducto(id: number) {
+    return this.db.executeSql('DELETE FROM productos WHERE id_prod = ?', [id]);
+  }
 }
